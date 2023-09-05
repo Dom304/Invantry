2327562f3ca33d61ab89e036b6de732dc1e73a75
hjrodrig <hjrodrig@uncg.edu>
2023-09-05 14:47:45 -0400

    necessary views created
    Based on the report, I have created files for each view for the Web Application.

---
+++ resources/views/public_getStartedPage.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Invantry</title>
+    <!-- Include Bootstrap CSS via CDN -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body class="d-flex align-items-center justify-content-center" style="height: 100vh; background-color: #f8f9fa;">
+    <div class="position-absolute" style="top: 25%;">
+        <img src="{{ asset('images/Button_backpack_logo.png') }}" alt="Invantry Logo" class="mx-auto d-block">
+    </div>
+</body>
+</html>
