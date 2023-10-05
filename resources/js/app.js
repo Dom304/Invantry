a2e398c7de79b54e1e666c5f34c7b0567801cd22
Dominique Simmons <dominiquesimmons304@gmail.com>
2023-10-05 08:06:40 -0400

    Converted login into vue component, moved styles into app.scss
    WIP: Need to adjust eye png

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,11 @@ import './bootstrap';
 
 import { createApp } from 'vue';
 import ExampleComponent from './components/ExampleComponent.vue';
+import LoginComponent from './components/Login.vue';
 
-const app = createApp({});
-app.component('example-component', ExampleComponent);
-app.mount('#app');
+document.addEventListener('DOMContentLoaded', () => {
+    const app = createApp({});
+    app.component('example-component', ExampleComponent);
+    app.component('login', LoginComponent);
+    app.mount('#app');
+});
---
+++ resources/js/components/Login.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="image-container">
+        <img :src="'/images/Button_backpack_logo.png'" alt="Invantry Logo" class="mx-auto d-block centered-image">
+        <div class="app-name">Invantry</div>
+        <div class="description">PERSONAL BELONGINGS MANAGEMENT SYSTEM</div>
+        <div class="welcome-back">WELCOME BACK</div>
+        <input type="text" class="username-input" placeholder="Username/Email address">
+        <div class="password-container">
+            <input :type="passwordFieldType" ref="passwordField" class="password-input" placeholder="Password">
+            <span @click="togglePassword" class="password-icon" :class="{ active: isPasswordVisible }">
+                <img :src="eyeIcon" alt="Toggle Password Visibility">
+            </span>
+
+        </div>
+        <button class="action-btn continue-btn">Continue</button>
+        <div class="sign-up">DON'T HAVE AN ACCOUNT? <a href="#" class="sign-up-link">SIGN UP</a></div>
+    </div>
+</template>
+  
+<script>
+export default {
+    name: 'Login',
+    data() {
+        return {
+            passwordFieldType: 'password',
+            isPasswordVisible: false,
+        };
+    },
+    computed: {
+        eyeIcon() {
+            return this.isPasswordVisible ? '/images/eye-open.png' : '/images/eye-closed.png';
+        }
+    },
+    methods: {
+        togglePassword() {
+            if (this.passwordFieldType === 'password') {
+                this.passwordFieldType = 'text';
+                this.isPasswordVisible = true;
+            } else {
+                this.passwordFieldType = 'password';
+                this.isPasswordVisible = false;
+            }
+        },
+    },
+};
+</script>
