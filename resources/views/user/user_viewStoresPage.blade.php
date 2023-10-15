39d830a41a9a24b93180c9e75286a53eaf403e7a
Dominique Simmons <dominiquesimmons304@gmail.com>
2023-10-15 13:16:39 -0400

    Fixed window layouts + styling for store cards.

--- resources/views/user/user_viewStoresPage.blade.php
+++ resources/views/user/user_viewStoresPage.blade.php
@@ -16,36 +16,78 @@
     </div>
 </div>
 
-    <div class="page-content">
+<div class="page-content">
         
-<div class="left-window">
-    <div class="user-info">
-        <span class="username">Username</span>
+    <div class="left-window">
+        <div class="user-info">
+            <span class="username">Username</span>
+        </div>
+        <button class="stores-btn" id="stores-user-btn">Stores</button>
+        <div class="playlist-search-container">
+            <input type="text" placeholder="Search Playlists..." class="playlist-search-bar" id="playlist-search-bar-input">
+        </div>
+        <button class="playlist-btn">Going Gym</button>
     </div>
-    <button class="stores-btn">Stores</button>
-    <div class="playlist-search-container">
-        <input type="text" placeholder="Search Playlists..." class="playlist-search-bar">
+
+    <div class="middle-window">
+        <!-- href="/stores/store-name" -->
+        <a href="#" class="store">
+            <span class="store-name">Lowe's</span>
+        </a>
+        <a href="#" class="store">
+            <span class="store-name">Best Buy</span>
+        </a>
+        <a href="#" class="store">
+            <span class="store-name">Five Below</span>
+        </a>
+        <a href="#" class="store">
+            <span class="store-name">Dick's Sporting Goods</span>
+        </a>
+        <a href="/stores/best-buy" class="store">
+            <span class="store-name">Best Buy</span>
+        </a>
+        <a href="#" class="store">
+            <span class="store-name">Five Below</span>
+        </a>
+        <a href="#" class="store">
+            <span class="store-name">Golf Galaxy</span>
+        </a>
+        <a href="#" class="store">
+            <span class="store-name">The Vitamin Shoppe</span>
+        </a>
+        <a href="#" class="store">
+            <span class="store-name">Gamestop</span>
+        </a>
+        <a href="#" class="store">
+            <span class="store-name">Spirit Halloween</span>
+        </a>
+        <a href="#" class="store">
+            <span class="store-name">Target</span>
+        </a>
+        <a href="#" class="store">
+            <span class="store-name">Harris Teeter</span>
+        </a>
+        <a href="#" class="store">
+            <span class="store-name">Food Lion</span>
+        </a>
+        <a href="#" class="store">
+            <span class="store-name">Ikea</span>
+        </a>
+        <a href="#" class="store">
+            <span class="store-name">Academy Sports + Outdoors</span>
+        </a>
+        <a href="#" class="store">
+            <span class="store-name">GetOutdoors Pedal & Paddle</span>
+        </a>
+        <a href="#" class="store">
+            <span class="store-name">Dollar Tree</span>
+        </a>
     </div>
-    <button class="playlist-btn">Going Gym</button>
+
+    <div class="right-window">
+        <!-- Content will be dynamically populated or can remain empty -->
+    </div>
+
 </div>
 
-        
-<div class="middle-window">
-    <div class="store">
-        <span class="store-name">Lowe's</span>
-    </div>
-    <div class="store">
-        <span class="store-name">Best Buy</span>
-    </div>
-    <div class="store">
-        <span class="store-name">Five Below</span>
-    </div>
-</div>
-
-        
-<div class="right-window">
-    <!-- Content will be dynamically populated or can remain empty -->
-</div>
-
-    </div>
 @endsection
