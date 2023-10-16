01c4227eebc520e2051e895c7cf55307b3b1247d
hjrodrig <hjrodrig@uncg.edu>
2023-10-16 01:55:36 -0400

    Added styling to Homepage side window & top header

--- resources/views/user/user_viewStoresPage.blade.php
+++ resources/views/user/user_viewStoresPage.blade.php
@@ -1,22 +1,27 @@
-
 @extends('layouts.app')
 
 @section('content')
     <div id="app">
         <homepage></homepage> <!-- This is your Vue component -->
     </div>
+    
+<div class="top-toolbar">
+    <div class="logo-container">
+        <img src="{{ asset('images/Button_backpack_logo.png') }}" alt="Website Logo" class="logo">
+        <span class="website-name">Invantry</span>
+    </div>
+    <div class="search-container">
+        <input type="text" placeholder="Search..." class="search-bar">
+    </div>
+    <div class="cart-container">
+        <button class="cart-btn">Cart</button>
+    </div>
+</div>
 
 <div class="page-content">
         
     <div class="left-window">
-        <div class="user-info">
-            <span class="username">Username</span>
-        </div>
-        <button class="stores-btn" id="stores-user-btn">Stores</button>
-        <div class="playlist-search-container">
-            <input type="text" placeholder="Search Playlists..." class="playlist-search-bar" id="playlist-search-bar-input">
-        </div>
-        <button class="playlist-btn">Going Gym</button>
+        
     </div>
 
     <div class="middle-window">
@@ -178,6 +183,7 @@
         <!-- Content will be dynamically populated or can remain empty -->
     </div>
 
+</div>
 </div>
 
 @endsection
