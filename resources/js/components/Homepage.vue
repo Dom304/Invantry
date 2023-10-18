69f47359d3ceb54b44b2c82bdef6948e77decc3a
Dominique Simmons <dominiquesimmons304@gmail.com>
2023-10-17 23:12:48 -0400

    Updated styling for the Username

--- resources/js/components/Homepage.vue
+++ resources/js/components/Homepage.vue
@@ -1,38 +1,38 @@
 <template>
-    <div>
+  <div>
 
-      <!-- HEADER / TOP TOOLBAR CONTENT -->
-      <header class="header">
-        <img src="/images/Button_backpack_logo.png" alt="Logo" class="logo" />
-        <h1 class="app-name">Invantry</h1>
-        <div class="search-container">
-          <input type="text" placeholder="Search items, products, and stores" class="search-input" />
-        </div>
-        <div class="cart-container">
-          <button class="cart-button" @click="onCartClick">
-            <img src="/images/cart_icon.png" alt="Cart" /> 
-          </button>
-        </div>
-      </header>
-
-        <!-- SIDE WINDOW CONTENT -->
-        <div class="left-window">
-          <div class="user-info">
-            <div class="user-img">
-              <i class="fa-solid fa-user"></i>
-            </div>
-            
-              <span class="username">USERNAME</span>
-          </div>
-          <button class="stores-btn" id="stores-user-btn">Stores</button>
-          <div class="playlist-search-container">
-              <input type="text" placeholder="Search Collections..." class="playlist-search-bar" id="playlist-search-bar-input">
-          </div>
-          <button class="playlist-btn">Going Gym</button>
+    <!-- HEADER / TOP TOOLBAR CONTENT -->
+    <header class="header">
+      <img src="/images/Button_backpack_logo.png" alt="Logo" class="logo" />
+      <h1 class="app-name">Invantry</h1>
+      <div class="search-container">
+        <input type="text" placeholder="Search items, products, and stores" class="search-input" />
       </div>
-  
-      <!-- Other homepage content goes here -->
+      <div class="cart-container">
+        <button class="cart-button" @click="onCartClick">
+          <img src="/images/cart_icon.png" alt="Cart" />
+        </button>
+      </div>
+    </header>
+
+    <!-- SIDE WINDOW CONTENT -->
+    <div class="left-window">
+      <div class="user-info">
+        <div class="user-img">
+          <i class="fa-solid fa-user"></i>
+        </div>
+
+        <span class="username">USERNAME</span>
+      </div>
+      <button class="stores-btn" id="stores-user-btn">Stores</button>
+      <div class="playlist-search-container">
+        <input type="text" placeholder="Search Collections..." class="playlist-search-bar" id="playlist-search-bar-input">
+      </div>
+      <button class="playlist-btn">Going Gym</button>
     </div>
+
+    <!-- Other homepage content goes here -->
+  </div>
 </template>
   
 <script>
@@ -52,114 +52,142 @@ export default {
 
     // METHODS USED BY SIDE WINDOW
   }
-  
+
 }
 </script>
 
 <style scoped>
-
 /*TOP TOOLBAR*/
-  .header {
-    display: flex;
-    width: auto;
-    justify-content: space-between;
-    align-items: center;
-    padding: 10px 30px;
-    background-color: #fff;
-    box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2);
-    
-    
-  }
+.header {
+  display: flex;
+  width: auto;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 30px;
+  background-color: #fff;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2);
 
-  .logo {
-    max-height: 30px; 
-    width: auto;
-    margin-right: 30px; 
-  }
 
-  .app-name {
-    margin-top: 10px; 
-    margin-right: 60px; 
-    font-size: 1.5em; 
-  }
+}
 
-  .search-container {
-    flex-grow: 2; /* Lets search container to take up more space */
-    margin-right: 220px;
-    display: flex;
-    align-items: center;
-  }
+.logo {
+  max-height: 30px;
+  width: auto;
+  margin-right: 30px;
+}
 
-  .search-input {
-    width: 100%;
-    padding: 10px;
-    border: 1px solid #ccc; 
-  }
+.app-name {
+  margin-top: 10px;
+  margin-right: 60px;
+  font-size: 1.5em;
+}
 
-  .cart-container {
-    margin-left: 20px; 
-  }
+.search-container {
+  flex-grow: 2;
+  /* Lets search container to take up more space */
+  margin-right: 220px;
+  display: flex;
+  align-items: center;
+}
 
-  .cart-button {
-    background-color: transparent;
-    border: none;
-    cursor: pointer;
-    padding: 5px;
-  }
+.search-input {
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #ccc;
+}
 
-  .cart-button img {
-    width: 30px; 
-    height: auto;
-  }
+.cart-container {
+  margin-left: 20px;
+}
+
+.cart-button {
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  padding: 5px;
+}
+
+.cart-button img {
+  width: 30px;
+  height: auto;
+}
 
 /*SIDE WINDOW*/
 
+.user-img {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  background-color: #333;
+  margin-right: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  i {
+    color: #fff;
+    font-size: 20px;
+  }
+}
+
 .left-window {
-    margin-top: 67px;
-    width: 273px; /* Adjust width as needed */
-    background-color: #fff; /* Adjust color as needed */
-    padding: 20px;
-    box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.2);
-    height: 100vh; /* Makes it full height */
-    overflow-y: auto; /* Allows scrolling */
-    position: fixed;/*  Keeps it in place */
-    top: 0;
-    left: 0;
-    z-index: 1000; /* Adjust as needed to ensure it's above other content */
+  margin-top: 67px;
+  width: 273px;
+  /* Adjust width as needed */
+  background-color: #fff;
+  /* Adjust color as needed */
+  padding: 20px;
+  box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.2);
+  height: 100vh;
+  /* Makes it full height */
+  overflow-y: auto;
+  /* Allows scrolling */
+  position: fixed;
+  /*  Keeps it in place */
+  top: 0;
+  left: 0;
+  z-index: 1000;
+  /* Adjust as needed to ensure it's above other content */
 }
 
 /* Additional styles for children elements if needed */
 .left-window .user-info {
-    display: flex; /* Establishes a flex context for its children */
-    justify-content: center; /* Centers children horizontally */
-    align-items: center; /* Centers children vertically */
-    height: 50px; /* Give it a height, or it will be as tall as its content */
-    margin-bottom: 20px; /* Adjust as needed */
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  height: 50px;
+  margin-top: 10px;
+  margin-bottom: 50px;
 }
 
 .left-window .username {
-    font-size: 1.2em; /* Adjust as needed */
-    color: #333; /* Adjust color as needed */
+  font-size: 1.2em;
+  /* Adjust as needed */
+  color: #333;
+  /* Adjust color as needed */
 }
 
 .left-window button {
-    width: 100%;
-    padding: 10px;
-    margin-bottom: 10px; /* Adjust as needed */
-    background-color: #333; /* Adjust color as needed */
-    color: white; /* Adjust color as needed */
-    border: none;
-    cursor: pointer;
-    text-align: left;
+  width: 100%;
+  padding: 10px;
+  margin-bottom: 10px;
+  /* Adjust as needed */
+  background-color: #333;
+  /* Adjust color as needed */
+  color: white;
+  /* Adjust color as needed */
+  border: none;
+  cursor: pointer;
+  text-align: left;
 }
 
 .left-window .playlist-search-container {
-    margin-bottom: 20px; /* Adjust as needed */
+  margin-bottom: 20px;
+  /* Adjust as needed */
 }
 
 .left-window .playlist-search-bar {
-    width: 100%;
-    padding: 10px;
+  width: 100%;
+  padding: 10px;
 }
-
 </style>
