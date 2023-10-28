bc2346a5e9ea65091a5292a555d529dc99655a91
hjrodrig <hjrodrig@uncg.edu>
2023-10-28 14:08:37 -0400

    nothing

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use App\Http\Controllers\CollectionItemController;
 
 /*
 Route::get('/', function () {
-    return view('user.user_viewCartPage');
+    return view('user.user_viewStoresPage');
 });
 */
 
