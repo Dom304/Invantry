d78184bff0db4f84dda62d73da4d2c90035b1214
Dominique Simmons <dominiquesimmons304@gmail.com>
2023-11-02 11:29:29 -0400

    Cleaned up commented out code

--- resources/views/admin/admin_dashboard.blade.php
+++ resources/views/admin/admin_dashboard.blade.php
@@ -3,29 +3,19 @@
 @section('content')
 
 <div class="admin-header">
+
   <div class="left-section-header">
     <div class="Invantry-logo">
       <img src="/images/Button_backpack_logo.png" alt="Logo" class="logo" />
       <h1 class="logo-header">Invantry</h1>
     </div>
-  </div>
-  <div class="right-section-header">
-    {{-- <div class="user-name-dropdown"> --}}
-      {{-- <button class="user-name-btn">Hi, {{$user->name}} <i class="arrow-down-icon"></i></button>
-      <div class="dropdown-content">
-        <a href="/profile">Profile</a>
-        <a href="/settings">Settings</a>
-        <div class="logout-form">
-            <form method="POST" action="{{ route('logout') }}">
-                @csrf
-                <button type="submit" class="logout-btn">Logout</button>
-            </form>
-        </div>
-      </div>
-    </div> --}}
 
+  </div>
+
+  <div class="right-section-header">
     <user-btn :username="{{ json_encode($user->name) }}"></user-btn>
   </div>
+  
 </div>
 
 <div class="admin-page-content">
