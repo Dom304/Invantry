6e1d06c11aaca492eb0b4b1f3e5e946c974498c8
Alexw6901 <a_wesley@uncg.edu>
2023-11-03 22:30:05 -0400

    making dev up to date

--- resources/views/moderator/moderator_dashboard.blade.php
+++ resources/views/moderator/moderator_dashboard.blade.php
@@ -19,5 +19,7 @@
 </div>
 
 <div class="admin-page-content">
-  <admin-dashboard :users="{{ json_encode($users) }}" :stores="{{json_encode($stores)}}" :logged-in-user-id="{{ auth()->id() }}"></admin-dashboard>
+  <moderator-dashboard :users="{{ json_encode($users) }}" :stores="{{json_encode($stores)}}" :logged-in-user-id="{{ auth()->id() }}"></admin-dashboard>
 </div>
+
+@endsection
