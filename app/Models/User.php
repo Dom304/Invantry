4560efa2338d8e7010324c1ada724f67075b78b9
Dominique <dominiquesimmons304@gmail.com>
2023-11-19 10:53:28 -0500

    Update User model fillable fields to include role (can now update roles)

--- app/Models/User.php
+++ app/Models/User.php
@@ -36,11 +36,8 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $fillable = ['name', 'email', 'password', 'role'];
+
 
     /**
      * The attributes that should be hidden for serialization.
