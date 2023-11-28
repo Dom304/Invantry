2904e91a124cbb896c4562b1cae3db39c139c005
Dominique Simmons <dominiquesimmons304@gmail.com>
2023-11-28 12:49:32 -0500

    Added very generic toast for successful action notificaiton admin/mod

--- resources/js/components/AdminDashboard.vue
+++ resources/js/components/AdminDashboard.vue
@@ -4,47 +4,29 @@
             <i class="fa-solid fa-angle-left"></i>
             <span>Back</span>
         </a> -->
-        <button
-            class="window-btn"
-            :class="{ selected: currentwindow === 'user' }"
-            @click="currentwindow = 'user'"
-        >
+        <button class="window-btn" :class="{ selected: currentwindow === 'user' }" @click="currentwindow = 'user'">
             Users
         </button>
-        <button
-            class="window-btn"
-            :class="{ selected: currentwindow === 'store' }"
-            @click="currentwindow = 'store'"
-        >
+        <button class="window-btn" :class="{ selected: currentwindow === 'store' }" @click="currentwindow = 'store'">
             Stores
         </button>
-        
-        <button
-            class="window-btn"
-            :class="{ selected: currentwindow === 'request' }"
-            @click="currentwindow = 'request'">
+
+        <button class="window-btn" :class="{ selected: currentwindow === 'request' }" @click="currentwindow = 'request'">
             Requests
         </button>
     </div>
 
     <div class="right-window-admin-dashboard">
-        <user-table v-if="currentwindow === 'user'" 
-                :users="users" 
-                :logged-in-user-id="loggedInUserId" 
-                ref="userTable"
-                :is-busy="isFetchingUsers"
-                @refresh-users="fetchUsers"></user-table>
+        <user-table v-if="currentwindow === 'user'" :users="users" :logged-in-user-id="loggedInUserId" ref="userTable"
+            :is-busy="isFetchingUsers" @refresh-users="fetchUsers"></user-table>
 
-        <store-table v-else-if="currentwindow === 'store'" 
-            :stores="stores" 
-            :is-busy="isFetchingStore" 
+        <store-table v-else-if="currentwindow === 'store'" :stores="stores" :is-busy="isFetchingStore"
             @refresh-stores="fetchStores"></store-table>
 
-        <request-table v-else-if="currentwindow === 'request'" 
-            :manager-requests="managerRequests" 
-            :is-busy="isFetchingRequests" 
-            @refresh-requests="fetchRequests"></request-table>
+        <request-table v-else-if="currentwindow === 'request'" :manager-requests="managerRequests"
+            :is-busy="isFetchingRequests" @refresh-requests="fetchRequests"></request-table>
     </div>
+    <Toast />
 </template>
 
 
@@ -52,6 +34,7 @@
 import StoreTable from "./StoreTable.vue";
 import UserTable from "./UserTable.vue";
 import RequestTable from "./RequestTable.vue";
+import Toast from 'primevue/toast';
 
 export default {
     name: "admin-dashboard",
@@ -65,6 +48,7 @@ export default {
         StoreTable,
         UserTable,
         RequestTable,
+        Toast,
     },
 
     created() {
@@ -74,9 +58,9 @@ export default {
     },
 
     mounted() {
-        
+
     },
-    
+
     data() {
         return {
             currentwindow: "user",
@@ -86,50 +70,54 @@ export default {
             isFetchingUsers: false,
             isFetchingRequests: false,
             isFetchingStore: false,
+            showToast: false,
         };
     },
     methods: {
-    async fetchUsers() {
-        this.isFetchingUsers = true;
-        try {
-            let response = await axios.get('/refresh', { params: { type: 'users' } });
-            // Update the users data property with the new data
-            this.users = response.data;
-        } catch (error) {
-            console.error("Error fetching users:", error);
-        } finally {
-            this.isFetchingUsers = false;
-        }
-    },
+        async fetchUsers() {
+            this.isFetchingUsers = true;
+            try {
+                let response = await axios.get('/refresh', { params: { type: 'users' } });
+                // Update the users data property with the new data
+                this.users = response.data;
+            } catch (error) {
+                console.error("Error fetching users:", error);
+            } finally {
+                this.$toast.add({ severity: 'success', summary: 'Success', detail: 'Action successful', life: 60000 });
+                this.isFetchingUsers = false;
+            }
+        },
 
-    async fetchStores() {
-        console.log("fetching stores");
-        this.isFetchingStore = true;
-        try {
-            let response = await axios.get('/refresh', { params: { type: 'stores' } });
-            // Update the users data property with the new data
-            this.stores = response.data;
-        } catch (error) {
-            console.error("Error fetching stores:", error);
-        } finally {
-            this.isFetchingStore = false;
-        }
-    },
+        async fetchStores() {
+            console.log("fetching stores");
+            this.isFetchingStore = true;
+            try {
+                let response = await axios.get('/refresh', { params: { type: 'stores' } });
+                // Update the users data property with the new data
+                this.stores = response.data;
+            } catch (error) {
+                console.error("Error fetching stores:", error);
+            } finally {
+                this.$toast.add({ severity: 'success', summary: 'Success', detail: 'Action successful', life: 60000 });
+                this.isFetchingStore = false;
+            }
+        },
 
         async fetchRequests() {
             console.log("fetching requests");
             this.isFetchingRequests = true;
-        try {
-            let response = await axios.get('/refresh', { params: { type: 'manager_requests' } });
-            // Update the users data property with the new data
-            this.managerRequests = response.data;
-        } catch (error) {
-            console.error("Error fetching requests:", error);
-        } finally {
-            this.isFetchingRequests = false;
-        }
+            try {
+                let response = await axios.get('/refresh', { params: { type: 'manager_requests' } });
+                // Update the users data property with the new data
+                this.managerRequests = response.data;
+            } catch (error) {
+                console.error("Error fetching requests:", error);
+            } finally {
+                this.$toast.add({ severity: 'success', summary: 'Success', detail: 'Action successful', life: 60000 });
+                this.isFetchingRequests = false;
+            }
         },
-}
+    }
 
 };
 </script>
